b0fa92c381e23cd877fea892d0d9879e949ab204
Hamza Susam <hamzassm18@gmail.com>
2025-04-12 17:15:59 +0300

    feat: application => MediatR installed. GetLeaveTypeDetailRequest.cs, GetLeaveTypeListRequest.cs and their handlers added.

--- HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
+++ HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -5,8 +5,11 @@ namespace HR.LeaveManagement.Application;
 
 public class ApplicationServicesRegistration
 {
-    public static void ConfigureApplicationServices(IServiceCollection services)
+    public static IServiceCollection ConfigureApplicationServices(IServiceCollection services)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        
+        return services;
     }
 }
