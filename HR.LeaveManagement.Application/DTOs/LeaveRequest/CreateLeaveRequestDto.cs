14d245db98747b1ac478031e733f7148b9723597
Hamza Susam <hamzassm18@gmail.com>
2025-04-14 23:45:01 +0300

    feat: application => CreateLeaveRequestDto.cs added. DTOs subfolder structure change

--- HR.LeaveManagement.Application/DTOs/LeaveRequest/CreateLeaveRequestDto.cs
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/CreateLeaveRequestDto.cs
@@ -1,16 +1,12 @@
 using HR.LeaveManagement.Application.DTOs.Common;
-using HR.LeaveManagement.Application.DTOs.LeaveType;
 
 namespace HR.LeaveManagement.Application.DTOs.LeaveRequest;
 
-public class LeaveRequestDto : BaseDto
+public class CreateLeaveRequestDto : BaseDto
 {
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public LeaveTypeDto LeaveType { get; set; }
     public int LeaveTypeId { get; set; }
     public DateTime RequestDate { get; set; }
-    public DateTime ActionDate { get; set; }
-    public bool? IsApproved { get; set; }
-    public bool IsCancelled { get; set; }
+    public string RequestComments { get; set; }
 }
