fe41a0e3a9fba7d8a15f40f6b5f3c86572ca064b
Hamza Susam <hamzassm18@gmail.com>
2025-04-20 23:33:31 +0300

    feat: application => new Dtos added

--- HR.LeaveManagement.Application/DTOs/LeaveRequest/UpdateLeaveRequestDto.cs
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/UpdateLeaveRequestDto.cs
@@ -1,6 +1,8 @@
+using HR.LeaveManagement.Application.DTOs.Common;
+
 namespace HR.LeaveManagement.Application.DTOs.LeaveRequest;
 
-public class CreateLeaveRequestDto
+public class UpdateLeaveRequestDto : BaseDto
 {
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
