d63c07134303d4861c07c3630f0d5628827ae135
Hamza Susam <hamzassm18@gmail.com>
2025-05-22 23:47:11 +0300

    feat: infrastructure-persistence => GenericRepository added

---
+++ HR.LeaveManagement.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,55 @@
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR.LeaveManagement.Persistence.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    private readonly LeaveManagementDbContext _dbContext;
+
+    public GenericRepository(LeaveManagementDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<T> GetAsync(int id)
+    {
+        var entity = await _dbContext.Set<T>().FindAsync(id);
+
+        return entity;
+    }
+
+    public async Task<IReadOnlyList<T>> GetAllAsync()
+    {
+        var entities = await _dbContext.Set<T>().ToListAsync();
+
+        return entities;
+    }
+
+    public async Task<bool> ExistsAsync(int id)
+    {
+        var entity = await GetAsync(id);
+        
+        return entity != null;
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _dbContext.AddAsync(entity);
+        await _dbContext.SaveChangesAsync();
+        
+        return entity;
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _dbContext.Entry(entity).State = EntityState.Modified;
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        _dbContext.Set<T>().Remove(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+}
